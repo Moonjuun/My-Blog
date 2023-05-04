45e89df4b5919fb175b214a4d444fc129db45725
Jun <cmoonjun11@gmail.com>
2023-05-04 17:31:55 +0900

    구입한 도메인으로 변경

--- site.config.ts
+++ site.config.ts
@@ -11,7 +11,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'MoonJun-Blog',
-  domain: 'choiii',
+  domain: 'choii.link',
   author: 'MoonJun',
 
   // open graph metadata (optional)
