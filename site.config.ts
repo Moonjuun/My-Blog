83d1c891bf30112dd7b9f1b6b031cf98539b9c13
Jun <cmoonjun11@gmail.com>
2023-05-04 15:28:15 +0900

    MyBlog 세팅

---
+++ site.config.ts
@@ -0,0 +1,66 @@
+import { siteConfig } from './lib/site-config'
+
+export default siteConfig({
+  //https://dazzling-park-2c1.notion.site/7ca1504395ee467688ec52ab2893b94a?v=06b25fd5bc424c5cab479e568805d6ea
+  // the site's root Notion page (required)
+  rootNotionPageId: '7ca1504395ee467688ec52ab2893b94a',
+
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: null,
+
+  // basic site info (required)
+  name: 'MoonJun-Blog',
+  domain: 'choiii',
+  author: 'MoonJun',
+
+  // open graph metadata (optional)
+  description: 'Example Next.js Notion Starter Kit Site',
+
+  // social usernames (optional)
+  // twitter: 'transitive_bs',
+  github: 'MoonJuun',
+  // linkedin: 'moonjun-choi-9bb022223/',
+  // mastodon: '#', // optional mastodon profile URL, provides link verification
+  // newsletter: '#', // optional newsletter URL
+  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  isPreviewImageSupportEnabled: true,
+
+  // whether or not redis is enabled for caching generated preview images (optional)
+  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
+  // environment variables. see the readme for more info
+  isRedisEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null,
+
+  // whether to use the default notion navigation style or a custom one with links to
+  // important pages
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'Home',
+      pageId: '7ca1504395ee467688ec52ab2893b94a'
+    },
+    {
+      title: 'Contact',
+      pageId: 'a968a31a151140cebac017b5db603c3c'
+    }
+  ]
+})
